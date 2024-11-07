c2939266d3b2b6b421978de5408eae20486db798
Altamash Shahid <altamashshahid93@gmail.com>
2024-11-07 12:44:48 +0530

    Create 2D_matrix.v

---
+++ 2D_matrix.v
@@ -0,0 +1,34 @@
+module matrix;
+  
+  int a[4][4]='{ '{1,2,3,4}, '{5,6,7,8}, '{9,10,11,12},{13,14,15,16}};
+  
+  initial
+    begin
+      for(int i=0;i<4;i++)
+        begin
+          for(int j=0;j<4;j++)
+            begin
+              $write("%0d",a[i][j]);
+            end
+          $display("");
+    end
+  end
+endmodule
+
+//////////////////////////////////////////////////
+
+module matrix;
+  int a[4][4]='{ '{1,2,3,4}, '{5,6,7,8}, '{9,10,11,12},{13,14,15,16}};
+  
+  initial
+    begin
+  	foreach(a[i])
+    begin
+      foreach(a[j])
+        begin
+          $write("%0d",a[i][j]);
+        end
+          $display("");
+    end
+    end
+endmodule
