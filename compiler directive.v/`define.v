ce7ed80dc9f45dd9daf093ff7413ffdb60836262
Altamash Shahid <altamashshahid93@gmail.com>
2024-12-26 10:02:39 +0530

    Update `define.v

--- compiler directive.v/`define.v
+++ compiler directive.v/`define.v
@@ -1,4 +1,9 @@
-//`define directive is used to define text(macros) substitution in verilog program
+/*
+1.Compiler directive is the command or instruction in our code that provides guidance to the compiler.
+2.Compiler is the software that translate your code into machine code
+3.`define directive is used to define text(macros) substitution in verilog program.
+4.it can be used both inside and outside module definitions.
+*/
 
 `define HOLD   2'b00
 `define RESET  2'b01
