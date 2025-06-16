139bcfc1c24be9e15a7a2422f433564fd40f6b80
Altamash Shahid <altamashshahid93@gmail.com>
2025-06-16 16:17:07 +0530

    Update 2D_matrix.v

--- 2D_matrix.v
+++ 2D_matrix.v
@@ -1,3 +1,4 @@
+//////////////Method-1/////////////////
 module matrix;
   
   int a[4][4]='{ '{1,2,3,4}, '{5,6,7,8}, '{9,10,11,12},'{13,14,15,16}};
@@ -15,7 +16,7 @@ module matrix;
   end
 endmodule
 
-//////////////////////////////////////////////////
+///////////////////Method-2///////////////////
 
 module matrix;
   int a[4][4]='{ '{1,2,3,4}, '{5,6,7,8}, '{9,10,11,12},'{13,14,15,16}};
@@ -32,3 +33,21 @@ module matrix;
     end
     end
 endmodule
+
+//////////////////Method-3//////////////
+module matrix;
+  int a[4][4]='{ '{1,2,3,4}, '{5,6,7,8}, '{9,10,11,12},'{13,14,15,16}};
+  
+  initial
+    begin
+  	foreach(a[i])
+      begin
+      foreach(a[i][j])
+        begin
+          $write("%0d",a[i][j]);
+        end
+          $display("");
+      end
+    end
+endmodule
+
